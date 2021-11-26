aab138c83053eaf6d195c92b72738304dcd20046
FXYFXZ <fxy@mail.ru>
2021-11-26 11:42:16 +0300

    Klakado kaj sxango

--- app/src/main/java/ru/fxy7ci/girtago/ClockFragment.kt
+++ app/src/main/java/ru/fxy7ci/girtago/ClockFragment.kt
@@ -6,9 +6,11 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import ru.fxy7ci.girtago.databinding.FragmentClockBinding
 
@@ -19,7 +21,7 @@ import ru.fxy7ci.girtago.databinding.FragmentClockBinding
 class ClockFragment : Fragment(R.layout.fragment_clock) {
     private var _binding: FragmentClockBinding? = null
     private val binding get() = _binding!!
-
+    lateinit var myView : MyRectangle
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +29,14 @@ class ClockFragment : Fragment(R.layout.fragment_clock) {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentClockBinding.inflate(inflater,container, false)
+
+        binding.btnNext.setOnClickListener(View.OnClickListener {
+            myView.radius = 50f
+            myView.invalidate()
+//            myView.visibility = View.GONE
+        })
+
+
         return binding.root
     }
 
@@ -42,23 +52,33 @@ class ClockFragment : Fragment(R.layout.fragment_clock) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.clockContainer.addView(MyRectangle(activity))
+        myView = MyRectangle(activity)
+        binding.clockContainer.addView(myView)
     }
 
 
-    private class MyRectangle(context: Context?) : View(context) {
+    class MyRectangle(context: Context?) : View(context) {
         var paint: Paint = Paint()
+        var centerX = 255f
+        var centerY = 255f
+        var radius = 20f
+
         override fun onDraw(canvas: Canvas) {
             paint.color = Color.GREEN
             val rect = Rect(100, 255, 200, 300)
             canvas.drawRect(rect, paint)
             paint.color = Color.BLUE
-            canvas.drawCircle(255f,255f, 20f,paint)
+            canvas.drawCircle(centerX,centerY, radius,paint)
 
 
         }
-    }
 
+        fun setMyDate(){
+            radius= 50f
+        }
+
+
+    }
 
 
 
