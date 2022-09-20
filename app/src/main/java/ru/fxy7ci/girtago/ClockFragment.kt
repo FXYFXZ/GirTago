c9bac5cc6f000915730343e561f9d75807882d55
fxyf <fxy@mail.ru>
2022-09-20 20:56:12 +0300

    Menu setup on fragments

--- app/src/main/java/ru/fxy7ci/girtago/ClockFragment.kt
+++ app/src/main/java/ru/fxy7ci/girtago/ClockFragment.kt
@@ -1,7 +1,6 @@
 package ru.fxy7ci.girtago
 
 import android.annotation.SuppressLint
-import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
--- app/src/main/java/ru/fxy7ci/girtago/SettingsFragment.kt
+++ app/src/main/java/ru/fxy7ci/girtago/SettingsFragment.kt
@@ -1,13 +1,16 @@
 package ru.fxy7ci.girtago
-import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import ru.fxy7ci.girtago.databinding.FragmentSettingsBinding
+
 
 /**
  * A simple [Fragment] subclass.
@@ -26,22 +29,49 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
         ControlsSetup()
         setHasOptionsMenu (true)
+
+        val lst : MutableList<String> = ArrayList()
+        lst.add("fsdfs1")
+        lst.add("fsdfs2")
+        lst.add("fsdfs3")
+        lst.add("fsdfs4")
+        lst.add("fsdfs5")
+
+
+        binding.spZone.adapter = ArrayAdapter(this.requireActivity(), R.layout.spin_row, R.id.weekofday, lst)
+        binding.spZone.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(activity, type, Toast.LENGTH_LONG).show()
+                println(type)
+            }
+        }
+
         return binding.root
     }
 
     fun ControlsSetup(){
         binding.fldAltitude.setText("Кри")
-        binding.btnZone.setOnClickListener{
-            val catNames = arrayOf("+1", "+2", "+3", "+4","+5","+6","+7")
-            val builder = AlertDialog.Builder(context)
-            builder.setTitle("Выберите кота")
-                .setItems(catNames
-                ) { dialog, which ->
-                    binding.fldAltitude.setText(catNames[which])
-                }
-            builder.create()
-            builder.show()
-        }
+//        binding.btnZone.setOnClickListener{
+//            val catNames = arrayOf("+1", "+2", "+3", "+4","+5","+6","+7")
+//            val builder = AlertDialog.Builder(context)
+//            builder.setTitle("Выберите кота")
+//                .setItems(catNames
+//                ) { dialog, which ->
+//                    binding.fldAltitude.setText(catNames[which])
+//                }
+//            builder.create()
+//            builder.show()
+//        }
 
     }
 
